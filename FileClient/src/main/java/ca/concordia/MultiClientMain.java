e34ed8be286de4abddcdf532212c4475265491c0
ltvypham <tuongvyxinhdep24@gmail.com>
2025-11-16 22:04:57 -0500

    modify for invalid value not go to the loop

--- FileClient/src/main/java/ca/concordia/MultiClientMain.java
+++ FileClient/src/main/java/ca/concordia/MultiClientMain.java
@@ -1,5 +1,6 @@
 package ca.concordia;
 
+import javax.swing.plaf.synth.SynthDesktopIconUI;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,55 +12,62 @@ public class MultiClientMain {
 
         System.out.println("\nMULTIPLE THREADING TEST\n");
         System.out.print("How many threads do you want to launch? ");
-        int count = sc.nextInt();
+
+        int count = 0;
+
+        while(true){
+            if(sc.hasNextInt()){
+                count = sc.nextInt();
+                sc.nextLine();
+                break;
+            } else {
+                System.out.print("Invalid number. Please enter from 1-5: ");
+                sc.nextLine();
+            }
+        }
 
         int mode = 0;
 
-        while(true) {
+        while (true) {
             System.out.println("Choose a mode:");
             System.out.println("1 — CREATE");
             System.out.println("2 — WRITE");
             System.out.println("3 — READ");
             System.out.println("4 — DELETE");
             System.out.println("5 — LIST");
+            System.out.println("6 — QUIT");
             System.out.print("Enter mode: ");
 
-            if (!sc.hasNextInt()) {
-                System.out.println("Invalid input");
-                sc.nextLine();
+            String input = sc.nextLine();
+
+            try {
+                mode = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number. Please enter from 1-5: ");
                 continue;
             }
 
-            mode = sc.nextInt();
-
-            switch (mode) {
-                case 1,2,3,4,5 ->
-                {
-                    System.out.println("Press ENTER after finish to disconnect all clients.");
-                    System.out.println("\nLaunching " + count + " clients...\n");
-                    //work best for more than 1000 thread can change for smaller test
-                    ExecutorService pool = Executors.newFixedThreadPool(500);
-
-                    for (int i = 0; i < count; i++) {
-                        pool.submit(new ClientRunnable(i, mode));
-                        Thread.sleep(5);
-                    }
-
-                    System.in.read(); // Wait for user
-
-                    // all the client send quit
-                    ClientRunnable.DISCONNECT = true;
-
-                    pool.shutdown();
-
-                    System.out.println("All clients disconnected.");
-                    break;
-                }
-                default -> {
-                    System.out.println("Invalid input. Try to run the program again.");
-                }
+            if (mode < 1 || mode > 6) {
+                System.out.println("Invalid mode. Please enter from 1-5: ");
+                continue;
             }
+            System.out.println("Press Enter after finish to end the program.");
             break;
         }
+            //work best for more than 1000 thread can change for smaller test
+            ExecutorService pool = Executors.newFixedThreadPool(500);
+            System.out.println("\nLaunching " + count + " clients...\n");
+
+            for (int i = 0; i < count; i++) {
+                pool.submit(new ClientRunnable(i, mode));
+                Thread.sleep(5);
+            }
+
+            pool.shutdown();
+            System.out.println("\nAll clients finished their operation.");
+            System.in.read();
+            sc.close();
+            System.out.println("All clients disconnected.");
+
+        }
     }
-}
